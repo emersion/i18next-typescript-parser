fad468bda62d0fb166c588f9b6b8d5087ed67a2a
Simon Ser <contact@emersion.fr>
2025-05-30 14:30:43 +0200

    Add support for string unions
    TypeScript is smart enough to figure out the list of strings which
    can be generated by a template.

--- test/basic/index.ts
+++ test/basic/index.ts
@@ -7,6 +7,7 @@ t('withDefaultValue', 'asdf');
 t('withDefaultValueAndNoopOptions', 'asdf', { count: 42 });
 t('withDefaultValueAndNsOptions', 'asdf', { ns: 'foo' });
 t('foo:overrideNs');
+t(`templateWithoutSubstitution`);
 
 const customT = getFixedT(null, 'custom', 'bar');
 customT('simple');
@@ -17,3 +18,12 @@ const multipleNsT = getFixedT(null, ['multipleNs', 'multipleNsFoo'] as const);
 multipleNsT('simple');
 multipleNsT('withNsOptions', { ns: 'multipleNsFoo' });
 multipleNsT('multipleNsFoo:overrideNs');
+
+const keyUnion: 'union.bar' | 'union.baz' = Math.random() > 0.5 ? 'union.bar' : 'union.baz';
+t(keyUnion);
+const getChoice = (): 'h' | 'i' => Math.random() > 0.5 ? 'h' : 'i';
+t(`unionTemplate.${getChoice()}.asdf`);
+t(`twoUnionTemplate.${getChoice()}.and.${getChoice()}`);
+customT(`unionWithPrefix.${getChoice()}`);
+customT(`customFoo:unionWithOverrideNs.${getChoice()}`);
+t(`ternary.${Math.random() > 0.5 ? 'bar' : 'baz'}`);
